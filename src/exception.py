4f1dce2756c1200cb4fbe5cb2bc9f2ecbfebc634
nadal001@plattsburgh.edu <HIram6117>
2025-01-10 02:30:29 -0500

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_details: sys):
     _, _, exc_tb = error_details.exc_info()
@@ -19,38 +20,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-
-if __name__=="__main__":
-        try:
-            a=1/0
-        except Exception as e:
-             logging.info("Divide by zero")
-             raise CustomException(e, sys)
-
-
-
-
-'''import sys
-
-
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in Python Script name[{0}] Line number [{1}] Error Message [{2}]".format(
-        file_name, exc_tb.tb_lineno,str(error)
-
-        return error_message
-    )
-
-    
-class CustomException(Exception):
-    def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
-        self.error_message=error_message_detail(error_message, error_detail=error_detail)
-
-
-    def __str__(self):
-        return self.error_message
-
-'''
